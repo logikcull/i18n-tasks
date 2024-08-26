a7a577b2db117730e128985ee8e8f20f9e429eaa
Brian Mock <briannealmock@gmail.com>
2024-08-26 10:03:19 -0400

    Update run message

--- lib/i18n/tasks/command/commander.rb
+++ lib/i18n/tasks/command/commander.rb
@@ -16,7 +16,7 @@ module I18n::Tasks
       end
 
       def run(name, opts = {})
-        log_stderr "#{Rainbow('#StandWith').bg(:blue)}#{Rainbow('Ukraine').bg(:yellow)}"
+        log_stderr "#{Rainbow('#Viva').fg(:aliceblue).bg(:blue)}#{Rainbow('Logikcull').fg(:blue).bg(:aliceblue)}"
         name = name.to_sym
         public_name = name.to_s.tr '_', '-'
         log_verbose "task: #{public_name}(#{opts.map { |k, v| "#{k}: #{v.inspect}" } * ', '})"
